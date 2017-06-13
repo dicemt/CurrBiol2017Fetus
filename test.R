83fe0f23517036aa5db8e91aeb991b89734dee4b
Daisuke MATSUYOSHI <dicemt@users.noreply.github.com>
2017-06-13 16:49:58 +0900

    Add values

--- test.R
+++ test.R
@@ -6,15 +6,17 @@ Fetus
 Fdata <- Fetus[8:11]
 colnames(Fdata) <- c("UpTowards", "UpAway", "InvTowards", "InvAway") # Rename columns
 # Age
-apply(Fetus[2], 2, min) # Consistent with p.e1
-apply(Fetus[2], 2, max) # Consistent with p.e1
-apply(Fetus[2], 2, mean) # Consistent with p.e1
+apply(Fetus[2], 2, min) # 23, Consistent with p.e1
+apply(Fetus[2], 2, max) # 254, Consistent with p.e1
+apply(Fetus[2], 2, mean) # 240.6154, Consistent with p.e1
 # Maternal Tissue Thickness 
-apply(Fetus[4], 2, mean) # Consistent with p.e1
-apply(Fetus[4], 2, sd) # Consistent with p.e1
+apply(Fetus[4], 2, mean) # 27.46667,Consistent with p.e1
+apply(Fetus[4], 2, sd) # 10.31844, Consistent with p.e1
 # Behavioral data
-apply(Fdata, 2, mean) # Inconsistent with p.1
-apply(Fdata, 2, sd) # Inconsistent with p.1
+apply(Fdata, 2, mean) # Inconsistent with p.1  UpTowards     UpAway InvTowards    InvAway 
+                                              #1.0256410  0.2564103  0.4358974  0.3589744 
+apply(Fdata, 2, sd) # Inconsistent with p.1    UpTowards     UpAway InvTowards    InvAway 
+                                              #1.0879043  0.4983102  0.5980174  0.5843205 
 
 # Test
 library(coin)
